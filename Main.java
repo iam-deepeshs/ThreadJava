03cd76b977b2e6f5d02c25df65c80d816eafeed5
Deepesh Srivastava <121711203+iam-deepeshs@users.noreply.github.com>
2025-04-18 23:14:50 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+// Name: DEEPESH SRIVASTAVA
+// PRN: 23070126028 
+// Batch: AIML A2
+
+public class Main {
+    public static void main(String[] args) {
+        // Starting threads using separate operations
+        ThreadOperations operations = new ThreadOperations();
+
+        operations.runThreadClass();
+        operations.runRunnableClass();
+    }
+}
